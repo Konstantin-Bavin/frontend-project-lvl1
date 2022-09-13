35df50af2a43dd4ef2c186bdb0594b6fbbf81226
Konstantin <kbavinser@gamil.com>
2022-09-13 12:45:10 +0300

    add brain-even

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 ### Hexlet tests and linter status:
+
 [![Actions Status](https://github.com/Konstantin-Bavin/frontend-project-lvl1/workflows/hexlet-check/badge.svg)](https://github.com/Konstantin-Bavin/frontend-project-lvl1/actions)
+
+https://asciinema.org/a/riSNDRSv8abuio3CbhFOPQQ8j
---
+++ bin/brain-even.js
@@ -0,0 +1,44 @@
+#!/usr/bin/env node
+
+import readlineSync from 'readline-sync';
+import cli from '../src/cli.js'
+
+console.log('Welcome to the Brain Games!');
+
+let userName = cli();
+
+let questionValue = [];
+
+const isEven = (num) => num % 2 === 0;
+const correctAnswer = (num) => isEven(num) ? 'yes' : 'no';
+
+for (let i = 0; i < 3; i++) {
+    questionValue.push(Math.floor(Math.random() * 100) + 1);
+}
+
+let count = 0
+
+const game = (questionValue, correctAnswer, name) => {
+    while(count < 3) {
+        let value = questionValue[count];
+
+        console.log(`Qusetion: ${value}`);  
+    
+        let userAnswer = readlineSync.question('Your answer: ');
+        
+        if (userAnswer === correctAnswer(value)) {
+            console.log('Correct!');
+            count += 1;
+        } else {
+            console.log(`\'${userAnswer}\' is wrong answer ;(. Correct answer was \'${correctAnswer(value)}\'`);
+            console.log('Let\'s try again, ' + name + '!');
+            break;
+        }
+    
+        if (count === 3) {
+            console.log('Congratulations, ' + name + '!');
+        }
+    }
+}
+
+game(questionValue, correctAnswer, userName);
